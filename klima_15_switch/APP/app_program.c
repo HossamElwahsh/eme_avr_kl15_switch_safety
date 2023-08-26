ce8411f7eba0ba99196c3a07830538fc842a15c8
Hossam elwahsh <hacker2894@gmail.com>
2023-08-26 15:33:58 +0300

    init commit

---
+++ klima_15_switch/APP/app_program.c
@@ -0,0 +1,6 @@
+/*
+ * app_program.c
+ *
+ * Created: 8/26/2023 3:29:07 PM
+ *  Author: hacke
+ */ 
---
+++ klima_15_switch/main.c
@@ -0,0 +1,17 @@
+/*
+ * klima_15_switch.c
+ *
+ * Created: 8/26/2023 3:00:27 PM
+ * Author : Team GUCALEX
+ */ 
+
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
